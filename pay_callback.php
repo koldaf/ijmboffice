6340ea211306cc0677151f9f925f165502ed92d5
Koldaf <Koldaf20@gmail.com>
2024-07-18 11:20:29 +0100

    Fixing pay attempt

--- pay_callback.php
+++ pay_callback.php
@@ -42,13 +42,7 @@ if('success' == $tranx->data->status){
 	$ref = $tranx->data->reference;
 	$j_val = dlookup('json_val', 'payment_attempttb', "ret_ref = '$ref'");
 	$j = json_decode(base64_decode($j_val));
-	
-	var_dump($j_val); 
-	
-	print_r('<p> </p>');
-
-	var_dump($tranx->data);
-	exit;
+	//var_dump($tranx->data);exit;
 	//check payment amount
 	//var_dump($tranx->data->customer->email); echo $j->email; exit;
 	//var_dump($tranx->data->customer->customer_code); exit;
