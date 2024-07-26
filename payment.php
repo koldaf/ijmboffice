b66711a95369d9620f760c46fc2a51cf071be127
Koldaf <Koldaf20@gmail.com>
2024-07-26 11:47:12 +0100

    fix

--- payment.php
+++ payment.php
@@ -4,11 +4,8 @@ if(isset($_POST['email'])){
 	$pay = make_payment_attempt($_POST['email'], $_POST['phone'], $_POST['surname'], $_POST['othernames'], $_POST['regno'], $_POST['fee_code'],'',$_POST['prog_id'],$_POST['sess']);
 	
 	print_r($pay);
-	
 	//header("Location: {$pay}");
-
 	exit;
-
 }
 if ($_GET['exist'] == 0) {
 	$regno = base64_decode($_GET['reg']);
